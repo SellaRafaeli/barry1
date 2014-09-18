7a90686661bac0fef584809c33525daa1b756d74
Sella Rafaeli <sella.rafaeli@gmail.com>
2014-09-18 22:01:57 +0300

    minor

--- app.rb
+++ app.rb
@@ -5,16 +5,16 @@ require 'json'
 
 Bundler.require
 
-puts "done requiring".blue
-
-# config
+# config - export to config.rb when expands
 $app_name = 'boilerApp1'
 
-# our app files
+# our app files - export to require.rb or st when expands
 require './db/mongo.rb'
 require './users/users_api.rb'
 	
-# sanity + let's get cooking
+# sanity + let's get cooking 
 get '/' do 
 	"This is #{$app_name}\n" 
 end
+
+puts "Ready to rock'n'roll".light_red
