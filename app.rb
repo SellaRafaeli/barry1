a32fdf99138046eaee5e6aa8ddcd8d3b223f9b60
Sella Rafaeli <sella.rafaeli@gmail.com>
2014-09-18 22:00:12 +0300

    minimalism

--- app.rb
+++ app.rb
@@ -11,8 +11,10 @@ puts "done requiring".blue
 $app_name = 'boilerApp1'
 
 # our app files
-require './config/config.rb'
 require './db/mongo.rb'
-require './base_api.rb'
 require './users/users_api.rb'
 	
+# sanity + let's get cooking
+get '/' do 
+	"This is #{$app_name}\n" 
+end
--- base_api.rb
+++
@@ -1,3 +0,0 @@
-get '/' do 
-	"pong, motherfucker\n" 
-end
